35716d6ab2fc947425a179d74036026813ae45ec
Miguel Machado <hello@miguelmachado.dev>
2025-09-23 20:08:56 -0300

    Add deck saving and update main to save

--- deck.go
+++ deck.go
@@ -1,6 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"strings"
+)
 
 type deck []string
 
@@ -27,4 +31,14 @@ func (d deck) print() {
 
 func deal(d deck, handSize int) (deck, deck) {
 	return d[:handSize], d[handSize:]
+}
+
+func (d deck) toString() string {
+	return strings.Join([]string(d), ",")
+}
+
+func (d deck) saveToFile(filename string) error {
+	err := os.WriteFile(filename, []byte(d.toString()), 0666)
+
+	return err
 }
