d4f225b4fcca2dcdce7bb291342632723ffa2510
Miguel Machado <hello@miguelmachado.dev>
2025-09-24 18:11:02 -0300

    feat:add deck shuffling and VSCode settings
    Update main to shuffle and print a deck. Disable Copilot in VSCode
    settings.

--- main.go
+++ main.go
@@ -1,11 +1,9 @@
 package main
 
 func main() {
-	// cards := newDeck()
+	cards := newDeck()
 
-	// cards.saveToFile("my_card")
-
-	cards := newDeckFromFile("my_cards")
+	cards.shuffle()
 
 	cards.print()
 }
